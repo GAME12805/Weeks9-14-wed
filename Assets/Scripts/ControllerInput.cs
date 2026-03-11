2b31410bf6e6bc79e2d376676f1819bde2b747a6
DESKTOP-111JM1H\Kit Barry <kit.barry@sheridancollege.ca>
2026-03-11 11:24:04 -0400

    Got the controller moving a player

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInput : MonoBehaviour
+{
+    public float speed = 5;
+    public Vector2 movement;
+    public AudioSource SFX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //use with a stick
+        //transform.position += (Vector3)movement * speed * Time.deltaTime;
+        //use wth the mouse position
+        transform.position = movement;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack! " + context.phase);
+        if(context.performed == true)
+        {
+            SFX.Play();
+        }
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        //The same as Mouse.current.position.ReadValue()
+        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+}
