e16ddd2af8f84df59c215a3b3a48f70fc86990a0
DESKTOP-111JM1H\Kit Barry <kit.barry@sheridancollege.ca>
2026-03-18 10:03:21 -0400

    Stopped coroutines! ansd got a coroutine to wait for anotehr coroutine to finish

---
+++ Assets/Scripts/Grower.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+
+public class Grower : MonoBehaviour
+{
+    public Transform tree;
+    public Transform apple;
+
+    Coroutine doTheGrowingCoroutine;
+    Coroutine growTheTreeCoroutine;
+    Coroutine growTheAppleCoroutine;
+    void Start()
+    {
+        tree.localScale = Vector2.zero;
+        apple.localScale = Vector2.zero;
+    }
+
+    public void StartTreeGrowing()
+    {
+        if(doTheGrowingCoroutine != null)
+        {
+            StopCoroutine(doTheGrowingCoroutine);
+        }
+        if (growTheTreeCoroutine != null)
+        {
+            StopCoroutine(growTheTreeCoroutine);
+        }
+        if(growTheAppleCoroutine != null)
+        {
+            StopCoroutine(growTheAppleCoroutine);
+        }
+        doTheGrowingCoroutine = StartCoroutine(DoTheGrowing());
+    }
+
+    IEnumerator DoTheGrowing()
+    {
+        yield return growTheTreeCoroutine = StartCoroutine(GrowTree());
+        yield return growTheAppleCoroutine = StartCoroutine(GrowApple());
+    }
+
+    IEnumerator GrowTree()
+    {
+        Debug.Log("Started growing Tree");
+        tree.localScale = Vector2.zero;
+        apple.localScale = Vector2.zero;
+        float t = 0;
+        while(t < 1)
+        {
+            t += Time.deltaTime;
+            tree.localScale = Vector2.one * t;
+            yield return null;
+        }
+        Debug.Log("Finished growing tree");
+    }
+    IEnumerator GrowApple()
+    {
+        Debug.Log("Started growing apple");
+        apple.localScale = Vector2.zero;
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            apple.localScale = Vector2.one * t;
+            yield return null;
+        }
+        Debug.Log("Finished growing apple");
+    }
+}
